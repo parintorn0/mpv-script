466246673109d63915216db97d5350026f91fbc2
parintorn0 <github.dallying824@slmail.me>
2024-09-01 02:57:15 +0700

    1.1

--- scripts/aPoint-bPoint_loop.lua
+++ scripts/aPoint-bPoint_loop.lua
@@ -2,6 +2,7 @@ local mp = require("mp")
 
 local aPoint=0
 local bPoint=nil
+local looping=true
 
 function setA()
     aPoint=mp.get_property_number("time-pos")
@@ -14,7 +15,7 @@ function setB()
 end
 
 function loop()
-    if bPoint~=nil and aPoint < bPoint and mp.get_property_number("time-pos") >= bPoint then
+    if bPoint~=nil and aPoint < bPoint and mp.get_property_number("time-pos") >= bPoint and looping then
         mp.set_property_native("time-pos", aPoint)
     end
 end
@@ -27,9 +28,26 @@ function resetB()
     bPoint=nil
 end
 
+function toggleLoop()
+    looping= not looping
+end
+
+function seektoHome()
+    mp.set_property_native("time-pos", 0)
+end
+
+function seektoEnd()
+    mp.set_property_native("time-pos", mp.get_property_number("duration"))
+end
+
 mp.observe_property("time-pos", "string", loop)
 
 mp.add_forced_key_binding("[", "[", setA)
 mp.add_forced_key_binding("]", "]", setB)
 mp.add_forced_key_binding("{", "{", resetA)
 mp.add_forced_key_binding("}", "}", resetB)
+mp.add_forced_key_binding("\\", "\\", toggleLoop)
+
+mp.add_forced_key_binding(",", "", seektoHome)
+mp.add_forced_key_binding(".", "", seektoEnd)
+
